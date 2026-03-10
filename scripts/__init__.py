a841c13946b80204581dc59710efdc57a398c99e
ishf9164-glitch <2745302895@qq.com>
2026-03-10 17:56:33 +0800

    add project files except dump

--- scripts/__init__.py
+++
@@ -1,9 +0,0 @@
-import os
-
-
-def ensure_parent_directory_exists(filepath):
-    os.makedirs(filepath, exist_ok=True)
-
-
-if __name__ == '__main__':
-    print(list(map(int, "512,512".split(","))))
--- synthesis/postprocess/matlab/test.m
+++
@@ -1,6 +0,0 @@
-clear;clc;close;
-
-data = load('21.mat');
-
-[newBox, types, edge] = align_fp(data.boundary, data.boxes, data.type, data.edges, 0.35, data.colors,true);
-
